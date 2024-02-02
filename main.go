d1371800dca03644b2186e96270f7289d66cb119
Rohan Sai Buddhi <rohan31sai@gmail.com>
2024-02-01 23:30:59 -0600

    Added simple http server in Go

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type movie struct {
+	Name     string
+	Director string
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Web server is active")
+}
 
 func main() {
 	fmt.Println("Hello World")
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
