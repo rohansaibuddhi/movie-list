6ab066a258d7ad756d7cfdefe9b16b08f26974b7
Rohan Sai Buddhi <rohan31sai@gmail.com>
2024-02-18 17:52:58 -0600

    Added HTML template and CSS

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 )
@@ -12,11 +13,20 @@ type movie struct {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("main.html"))
+	tmpl.Execute(w, nil)
 	fmt.Println("Web server is active")
 }
 
 func main() {
 	fmt.Println("Hello World")
+
+	//handle fetching static css
+	fs := http.FileServer(http.Dir("mystyles"))
+	//fetch the mystyles directory, and strip the prev dir
+	// Serve static files in the mystyles directory
+	http.Handle("/mystyles/", http.StripPrefix("/mystyles/", fs))
+
 	http.HandleFunc("/", handler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
