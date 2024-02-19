c3db8410330f9e2c844b439527fc3a82e552ae18
Rohan Sai Buddhi <rohan31sai@gmail.com>
2024-02-18 19:51:32 -0600

    Added form functionality and HTMX for dynamic update of page

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"time"
 )
 
 type Film struct {
@@ -26,6 +27,17 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Web server is active")
 }
 
+func handler2(w http.ResponseWriter, r *http.Request) {
+	time.Sleep(1 * time.Second)
+	title := r.PostFormValue("title")
+	director := r.PostFormValue("director")
+
+	//create a new template which will be list item
+	htmlstr := fmt.Sprintf("<li class = 'list-group-item bg-primary text-white'> %s - %s</li>", title, director)
+	tmpl, _ := template.New("t").Parse(htmlstr)
+	tmpl.Execute(w, nil)
+}
+
 func main() {
 	fmt.Println("Hello World")
 
@@ -36,5 +48,6 @@ func main() {
 	http.Handle("/mystyles/", http.StripPrefix("/mystyles/", fs))
 
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/add-film/", handler2)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
